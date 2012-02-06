679d2fcf8a7e2ea7e6c3233acad63e08e0c71452
Luis Dario Simonassi <ldsimonassi@gmail.com>
2012-02-06 12:42:42 -0300

    convert some tests to groovy

--- src/test/groovy/individual/MergerTest.groovy
+++ src/test/groovy/individual/MergerTest.groovy
@@ -1,4 +1,5 @@
 package individual
+
 import org.junit.Test;
 import search.MergeBolt.Merger
 import search.model.Item
@@ -9,16 +10,17 @@ public class MergerTest extends Assert  {
 
 	@Test
 	public void mergerMaintainsMaxSize() {
-		ArrayList<Item> a= new ArrayList<Item>();
-		ArrayList<Item> b= new ArrayList<Item>();
-		ArrayList<Item> c= new ArrayList<Item>();
-		long id=0;
-		for(int i=0; i<10;i++){
-			a.add(new Item(id, "a", i));
+		def a = new ArrayList<Item>()
+		def b = new ArrayList<Item>()
+		def c = new ArrayList<Item>()
+		long id = 0
+
+		for(int i=0; i<10; i++) {
+			a.add(new Item(id, "a", i))
 			id++;
-			b.add(new Item(id, "b", i+0.25));
+			b.add(new Item(id, "b", i+0.25))
 			id++;
-			c.add(new Item(id, "c", i+0.5));
+			c.add(new Item(id, "c", i+0.5))
 			id++;
 		}
 		
@@ -33,10 +35,11 @@ public class MergerTest extends Assert  {
 	
 	@Test
 	public void mergerCorrectSort() {
-		ArrayList<Item> a= new ArrayList<Item>();
-		ArrayList<Item> b= new ArrayList<Item>();
-		ArrayList<Item> c= new ArrayList<Item>();
+		def a= new ArrayList<Item>();
+		def b= new ArrayList<Item>();
+		def c= new ArrayList<Item>();
 		long id=0;
+
 		for(int i=0; i<10;i++){
 			a.add(new Item(id, "a", i));
 			id++;
@@ -49,19 +52,13 @@ public class MergerTest extends Assert  {
 		Merger m= new Merger("localhost", "44", 5);
 		
 		m.merge(a);
-		println m.getResults()
-
 		m.merge(b);
-		println m.getResults()
-		
 		m.merge(c);
-		println m.getResults()
+
 		assertEquals(9.5, m.getResults().get(0).price, 0.01)
 		assertEquals(9.25, m.getResults().get(1).price, 0.01)
 		assertEquals(9, m.getResults().get(2).price, 0.01)
 		assertEquals(8.5, m.getResults().get(3).price, 0.01)
 		assertEquals(8.25, m.getResults().get(4).price, 0.01)
 	}
-
-
 }
--- src/test/groovy/individual/SearchBucketTests.groovy
+++ src/test/groovy/individual/SearchBucketTests.groovy
@@ -8,19 +8,19 @@ import org.junit.Assert
 public class SearchBucketTest extends Assert {
 	@Test
 	public void searchShard() {
-		ItemsShard myShard= new ItemsShard(20);
+		def myShard= new ItemsShard(20)
 		
-		Item a= new Item(0, "nice dvd player with usb and card reader", 100);
-		Item b= new Item(1, "new laptop computer with dvd and usb and manual", 100);
-		Item c= new Item(2, "elegant cell phone with usb charger manual dvd included", 100);
-		Item d= new Item(3, "new balck microwave includes cooking book and operation manual", 100);
+		Item a= new Item(0, "nice dvd player with usb and card reader", 100)
+		Item b= new Item(1, "new laptop computer with dvd and usb and manual", 100)
+		Item c= new Item(2, "elegant cell phone with usb charger manual dvd included", 100)
+		Item d= new Item(3, "new balck microwave includes cooking book and operation manual", 100)
 		
-		myShard.add(a);
-		myShard.add(b);
-		myShard.add(c);
-		myShard.add(d);
+		myShard.add a
+		myShard.add b
+		myShard.add c
+		myShard.add d
 		
-		Set<Item> result= myShard.getItemsContainingWords("nice-dvd");
+		def result= myShard.getItemsContainingWords("nice-dvd")
 		assert result.contains(a);
 		assert !result.contains(b);
 		assert !result.contains(c);
